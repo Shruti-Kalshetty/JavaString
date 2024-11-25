17efb43b03d2cbaf74097001a04c23ac2802ef71
Shruti Rajkumar Kalshetty <shrutikalshetty07@gmail.com>
2024-11-25 12:53:21 +0530

    Add files via upload

---
+++ PowerOfTwo.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class PowerOfTwo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number till that you want to print the table of 2");
+        int power = sc.nextInt();
+        if(power<=37) {
+            for (int i = 0; i <= power; i++) {
+                int ans = 2 * i;
+                System.out.println("2^" + i + "=" + ans);
+            }
+        }
+    }
+}
