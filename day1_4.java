ce3beb057fb29f15571009b39cb39ed6ac4d6a57
Shruti Rajkumar Kalshetty <shrutikalshetty07@gmail.com>
2024-12-11 16:52:35 +0530

    Add files via upload

---
+++ day1_4.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class day1_4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter 1st number");
+        int a = sc.nextInt();
+        System.out.println("Enter 2nd number");
+        int b = sc.nextInt();
+        int c= a+b;
+        //if ( a && b != integer )
+    }
+}
